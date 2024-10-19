c9bc8753b4797b3341c83e89162a75877c9f478b
nick <belyaev.frog01@gmail.com>
2024-10-19 13:13:59 +0300

    refactor

--- labwork/01_schema_and_db_setup/query/drop_tables.sql
+++
@@ -1,9 +0,0 @@
-drop extension if exists "uuid-ossp" cascade;
-
-drop table if exists "client" cascade;
-drop table if exists "trainer" cascade;
-drop table if exists "client_trainer" cascade;
-drop table if exists "gym" cascade;
-drop table if exists "equipment" cascade;
-drop table if exists "subscription" cascade;
-
--- labwork/01_schema_and_db_setup/script/generator/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/humanbelnik/db-class/01_init
-
-go 1.22.3
-
-require (
-	github.com/brianvoe/gofakeit/v7 v7.0.4 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-)
